8102aa743bfe03ff92ac64d61fab2c20efdf2e02
Atilla <Atillavatankhah@gmail.com>
2025-04-24 13:36:09 +0330

    fix picture

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const portfolioItems = [
     {
         id: 1,
         title: 'Chess Game',
-        category: 'web',
-        image: 'images/chess.jpg',
+        category: 'web'
+        image: 'images/chess.JPG',
         description: 'An interactive chess game built with HTML, CSS, and JavaScript. Features include move validation, piece highlighting, and game state tracking.',
         link: 'https://atillavatankhah.github.io/chess-game/'
     },
